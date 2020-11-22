06ae13d46412dde6757cc42a35116936b2c1148d
Robert Krone <rckrone@gmail.com>
2020-11-21 21:54:51 -0500

    a new executable appears!

---
+++ trials/death/death.cpp
@@ -0,0 +1,13 @@
+//
+// Created by robert on 11/21/20.
+//
+#include <iostream>
+
+int main() {
+    for(int i = 0; i < 10; i++) {
+        std::cout << "Good bye." << std::endl;
+    }
+    std::cout << "Good bye!" << std::endl;
+    return 0;
+}
+
